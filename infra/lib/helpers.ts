7783a050c57a1b22e47d50b7bd740f45f0df15b2
SanferD <sanferdsouza@gmail.com>
2024-05-10 19:16:27 -0500

    Add infra/lib/helpers make helpers

--- infra/lib/helpers.ts
+++ infra/lib/helpers.ts
@@ -1,5 +1,32 @@
 import * as constants from "./constants";
 import * as path from "path";
+import { execSync } from "child_process";
+
+const validMakeTargets = ["clean", "build", "build-trigger_crawler", "build-crawler"];
+
+export function codeClean() {
+  makeCode("clean");
+}
+
+export function codeBuild(name?: string) {
+  let target = name === null ? "build" : `build-${name}`;
+  makeCode(target);
+}
+
+export function makeCode(target: string) {
+  if (!validMakeTargets.includes(target)) {
+    throw new Error(`target '${target}' is not recognized`);
+  }
+  const rootDir = getRepositoryDirectory();
+  const codeDir = path.join(rootDir, "code");
+  execSync(`cd ${codeDir} && make ${target}`);
+}
+
+export function getAssetPath(target: string) {
+  let rootPath = getRepositoryDirectory();
+  let buildPath = path.join(rootPath, "code", ".build");
+  return path.join(buildPath, target, `${target}.zip`);
+}
 
 export function getRepositoryDirectory(): string {
   let directory = __dirname;
