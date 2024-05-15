6362691591c16f3ff6e10a9db2c2482f45c638e6
SanferD <sanferdsouza@gmail.com>
2024-05-15 01:19:31 -0500

    Add GetTestFile,DeleteTextFile

--- code/infrastructure/stores/s3.go
+++ code/infrastructure/stores/s3.go
@@ -48,6 +48,33 @@ func (s3Helper *S3Helper) PutTextFile(ctx context.Context, fileName string, body
 	return nil
 }
 
+func (s3Helper *S3Helper) GetTextFile(ctx context.Context, fileName string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, s3Helper.timeout)
+	defer cancel()
+	key := s3Helper.getRawObjectKey(fileName)
+	getObjectInput := &s3.GetObjectInput{Bucket: aws.String(s3Helper.bucketName), Key: aws.String(key)}
+	getObjectOutput, err := s3Helper.client.GetObject(ctx, getObjectInput)
+	if err != nil {
+		return "", fmt.Errorf("error on get object from s3: %v", err)
+	}
+	bytes, err := io.ReadAll(getObjectOutput.Body)
+	if err != nil {
+		return "", fmt.Errorf("error on reading contents of get object body: %v", err)
+	}
+	return string(bytes), nil
+}
+
+func (s3Helper *S3Helper) DeleteTextFile(ctx context.Context, fileName string) error {
+	ctx, cancel := context.WithTimeout(ctx, s3Helper.timeout)
+	defer cancel()
+	key := s3Helper.getRawObjectKey(fileName)
+	deleteObjectInput := &s3.DeleteObjectInput{Bucket: aws.String(s3Helper.bucketName), Key: aws.String(key)}
+	if _, err := s3Helper.client.DeleteObject(ctx, deleteObjectInput); err != nil {
+		return fmt.Errorf("error on deleting object from s3: %v", err)
+	}
+	return nil
+}
+
 func (s3Helper *S3Helper) getRawObjectKey(fileName string) string {
 	return s3Helper.rawPathPrefix + "/" + fileName
 }
