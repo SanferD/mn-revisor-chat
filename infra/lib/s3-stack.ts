b76087249454226a14d945a43a873818d768b444
SanferD <sanferdsouza@gmail.com>
2024-05-14 23:44:18 -0500

    Update s3-stack to use main-bucket

--- infra/lib/s3-stack.ts
+++ infra/lib/s3-stack.ts
@@ -2,25 +2,25 @@ import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
-export const RAW_OBJECT_PREFIX = "raw";
-export const TRANSFORMED_OBJECT_PREFIX = "transformed/";
+export const MAIN_BUCKET_NAME = "main-bucket";
 
 export interface S3StackProps extends cdk.StackProps {
   nonce: string;
 }
 
 export class S3Stack extends cdk.Stack {
-  readonly crawlerBucket: s3.Bucket;
+  readonly mainBucket: s3.Bucket;
 
   constructor(scope: Construct, id: string, props: S3StackProps) {
     super(scope, id, props);
 
     // create s3 bucket
-    this.crawlerBucket = new s3.Bucket(this, "crawler-bucket", {
+    this.mainBucket = new s3.Bucket(this, MAIN_BUCKET_NAME, {
+      bucketName: `${MAIN_BUCKET_NAME}-${props.nonce}`,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // for easy cleanup of demo
-      bucketName: `crawler-bucket-${props.nonce}`,
+      eventBridgeEnabled: true,
     });
   }
 }
