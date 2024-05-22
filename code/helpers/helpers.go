24c2f59e6589c968f9c80964b8f52c1a3e6937e3
SanferD <sanferdsouza@gmail.com>
2024-05-22 17:37:49 -0500

    Add Helper Function to Convert Statutes to Subdivision Chunks
    Implemented Statute2SubdivisionChunks function in helpers.go to convert statutes to subdivision chunks

--- code/helpers/helpers.go
+++ code/helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"code/core"
 	"encoding/base64"
 	"strings"
 )
@@ -12,4 +13,33 @@ func IsLocalhostURL(url string) bool {
 
 func Base64Encode(content string) string {
 	return base64.StdEncoding.EncodeToString([]byte(content))
+}
+
+func Statute2SubdivisionChunks(statute core.Statute) []core.Chunk {
+	var chunks []core.Chunk = make([]core.Chunk, 0)
+	id := statute.Chapter + "." + statute.Section
+	for _, subdivision := range statute.Subdivisions {
+		var idSubdiv string = id
+		if len(subdivision.Number) > 0 {
+			idSubdiv = idSubdiv + "." + subdivision.Number
+		}
+		var builder strings.Builder
+		builder.WriteString(idSubdiv)
+		builder.WriteString(": ")
+		builder.WriteString(statute.Title)
+		if len(subdivision.Heading) > 0 {
+			builder.WriteString(" -- ")
+			builder.WriteString(subdivision.Heading)
+		}
+		builder.WriteString("\n")
+		builder.WriteString(subdivision.Content)
+		if !strings.HasSuffix(subdivision.Content, "\n") {
+			builder.WriteString("\n")
+		}
+
+		chunk := core.Chunk{ID: idSubdiv, Body: builder.String()}
+		chunks = append(chunks, chunk)
+	}
+	return chunks
+
 }
