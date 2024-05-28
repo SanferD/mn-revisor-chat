78b4d88f04a0f7c8a4cd5f0daac9aa4d7b61c1a1
SanferD <sanferdsouza@gmail.com>
2024-05-28 13:46:08 -0500

    Organize settings, set default embedding, include all in emptysettings

--- code/infrastructure/settings/settings.go
+++ code/infrastructure/settings/settings.go
@@ -15,38 +15,44 @@ const relativeSettingsFilePath = "../../../settings.env"
 const defaultContextTimeout = 59 * time.Second
 
 type Settings struct {
-	MainBucketName           string        `mapstructure:"MAIN_BUCKET_NAME"`
-	ChunkPathPrefix          string        `mapstructure:"CHUNK_PATH_PREFIX"`
-	ContextTimeout           time.Duration `mapstructure:"CONTEXT_TIMEOUT"`
-	DoLogToStdout            bool          `mapstructure:"LOG_TO_STDOUT"`
-	LocalEndpoint            *string       `mapstructure:"LOCAL_ENDPOINT"`
-	RawEventsSQSARN          string        `mapstructure:"RAW_EVENTS_SQS_ARN"`
-	RawPathPrefix            string        `mapstructure:"RAW_PATH_PREFIX"`
-	Table1ARN                string        `mapstructure:"TABLE_1_ARN"`
-	URLSQSARN                string        `mapstructure:"URL_SQS_ARN"`
-	TriggerCrawlerTaskDfnArn string        `mapstructure:"TRIGGER_CRAWLER_TASK_DFN_ARN"`
-	TriggerCrawlerClusterArn string        `mapstructure:"TRIGGER_CRAWLER_CLUSTER_ARN"`
-	SubnetIds                []string      `mapstructure:"PRIVATE_ISOLATED_SUBNET_IDS"`
-	SecurityGroupIds         []string      `mapstructure:"SECURITY_GROUP_IDS"`
-	BedrockAgentID           string        `mapstructure:"BEDROCK_AGENT_ID"`
-	BedrockAgentAliasID      string        `mapstructure:"BEDROCK_AGENT_ALIAS_ID"`
+	ContextTimeout time.Duration `mapstructure:"CONTEXT_TIMEOUT"`
+	DoLogToStdout  bool          `mapstructure:"LOG_TO_STDOUT"`
+	LocalEndpoint  *string       `mapstructure:"LOCAL_ENDPOINT"`
+	// s3
+	MainBucketName  string `mapstructure:"MAIN_BUCKET_NAME"`
+	ChunkPathPrefix string `mapstructure:"CHUNK_PATH_PREFIX"`
+	RawPathPrefix   string `mapstructure:"RAW_PATH_PREFIX"`
+	// sqs
+	URLSQSARN       string `mapstructure:"URL_SQS_ARN"`
+	RawEventsSQSARN string `mapstructure:"RAW_EVENTS_SQS_ARN"`
+	// ddb
+	Table1ARN string `mapstructure:"TABLE_1_ARN"`
+	// ecs
+	TriggerCrawlerTaskDfnArn string   `mapstructure:"TRIGGER_CRAWLER_TASK_DFN_ARN"`
+	TriggerCrawlerClusterArn string   `mapstructure:"TRIGGER_CRAWLER_CLUSTER_ARN"`
+	SubnetIds                []string `mapstructure:"PRIVATE_ISOLATED_SUBNET_IDS"`
+	SecurityGroupIds         []string `mapstructure:"SECURITY_GROUP_IDS"`
+	// bedrock
+	EmbeddingModelID string `mapstructure:"EMBEDDING_MODEL_ID"`
 }
 
 const emptySettings = `
+CONTEXT_TIMEOUT=
+LOG_TO_STDOUT=
+LOCAL_ENDPOINT=
 MAIN_BUCKET_NAME=
 CHUNK_PATH_PREFIX=
-LOCAL_ENDPOINT=
-RAW_EVENTS_SQS_ARN=
 RAW_PATH_PREFIX=
-TABLE_1_ARN=
 URL_SQS_ARN=
+RAW_EVENTS_SQS_ARN=
+TABLE_1_ARN=
 TRIGGER_CRAWLER_TASK_DFN_ARN=
 TRIGGER_CRAWLER_CLUSTER_ARN=
 PRIVATE_ISOLATED_SUBNET_IDS=
 SECURITY_GROUP_IDS=
-BEDROCK_AGENT_ID=
-BEDROCK_AGENT_ALIAS_ID=
+EMBEDDING_MODEL_ID=
 `
+const defaultEmbeddingModelID = "amazon.titan-embed-text-v2:0"
 
 func GetSettings() (*Settings, error) {
 	viper.SetConfigType("env")
@@ -55,6 +61,7 @@ func GetSettings() (*Settings, error) {
 	viper.SetDefault("LOG_TO_STDOUT", true)
 	viper.SetDefault("TRIGGER_CRAWLER_TASK_DFN_ARN", "")
 	viper.SetDefault("TRIGGER_CRAWLER_CLUSTER_ARN", "")
+	viper.SetDefault("EMBEDDING_MODEL_ID", defaultEmbeddingModelID)
 
 	// load settings
 
